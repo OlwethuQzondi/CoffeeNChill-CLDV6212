6c7018f90c1a3a6dd7451dd7d27128e6bbc394d7
emeris <emeris@LabVM2302769>
2026-09-13 16:00:15 +0200

    Add project files.

---
+++ CoffeeNChillFunctions/DTOs/MenuItemDto.cs
@@ -0,0 +1,26 @@
+﻿namespace CoffeeNChillFunctions.DTOs
+{
+    public class MenuItemDto
+    {
+        public string Category { get; set; }
+        public string Sku { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+
+    public class UpdateMenuItemDto
+    {
+        public double Price { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+
+    public class StaffDocumentDto
+    {
+        public string FileName { get; set; }
+        public long SizeInBytes { get; set; }
+        public string ContentType { get; set; }
+        public System.DateTimeOffset? LastModified { get; set; }
+    }
+}
